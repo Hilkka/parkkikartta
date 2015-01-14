aac43f7bef41f8e75d683d0bed339fbf1af14a2c
vUksi <vUksi@users.noreply.github.com>
2015-01-14 16:00:32 +0200

    Revert "test commit"

--- ParkkiMap/MainPage.xaml.cs
+++ ParkkiMap/MainPage.xaml.cs
@@ -44,10 +44,5 @@ namespace ParkkiMap
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
